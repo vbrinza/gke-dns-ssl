90906ae6a3efc1e876c7a687e7d023e6ceda930d
Vitalie Brinza <231pithagora079@gmail.com>
2020-07-15 00:59:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,25 @@ resource "google_container_cluster" "primary" {
   }
 }
 
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/kubeconfig-template.yaml")
+
+  vars = {
+    cluster_name    = google_container_cluster.primary.name
+    user_name       = google_container_cluster.primary.master_auth[0].username
+    user_password   = google_container_cluster.primary.master_auth[0].password
+    endpoint        = google_container_cluster.primary.endpoint
+    cluster_ca      = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+    client_cert     = google_container_cluster.primary.master_auth[0].client_certificate
+    client_cert_key = google_container_cluster.primary.master_auth[0].client_key
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = data.template_file.kubeconfig.rendered
+  filename = "${path.module}/kubeconfig"
+}
+
 resource "google_compute_global_address" "test_address" {
   name = "${var.ip_address_name}"
 }
