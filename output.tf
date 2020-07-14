7f37c7ebf4908b33a31a2fe71fb0964803edbade
Vitalie Brinza <231pithagora079@gmail.com>
2020-07-15 00:59:22 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -17,3 +17,12 @@ output "endpoint" {
 output "node_version" {
   value = "${google_container_cluster.primary.node_version}"
 }
+
+output "kubeconfig_path" {
+  value = local_file.kubeconfig.filename
+}
+
+output "google_zone" {
+  value = data.google_compute_zones.available.names[0]
+}
+
